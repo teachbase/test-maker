def5ccd45cfa5aacbd161aa4e7d15504a196a7ca
Earendil95 <ermak95@gmail.com>
2016-04-01 20:51:23 +0300

    add specs

--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -4,11 +4,27 @@ describe Service do
   include Service
 
   describe "convert" do
-    let(:input) { File.read(file_path "input1") }
-    let(:output) { File.read file_path "output1" }
-    
-    subject { convert(input) }
+    let(:options) { {} }
+    subject { convert(input, options) }
 
-    it { expect(subject).to eq output }
+    context "default" do
+      let(:input) { File.read(file_path "input1") }
+      let(:output) { File.read file_path "output1" }
+
+      it { expect(subject).to eq output }
+    end
+
+    context "with options" do
+      let(:input) { File.read(file_path "input2") }
+      let(:output) { File.read file_path "output2" }
+
+      let(:options) do
+        { question_prefix: '^\s*\d+\.',
+          option_prefix: '^\s*\d\)',
+          option_correct: '(\+?)' }
+      end
+
+      it { expect(subject).to eq output }
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require_relative "../service"
+require 'pry-byebug'
 
 def file_path(name)
   "#{__dir__}/fixtures/#{name}.txt"
