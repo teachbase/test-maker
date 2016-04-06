6f3bbf4f8a44139aa6875e3ba00baa5f564fd511
Earendil95 <ermak95@gmail.com>
2016-04-06 15:01:23 +0300

    Add layout & js

--- controller.rb
+++ controller.rb
@@ -6,28 +6,33 @@ Cuba.define do
   on get do
     on "new" do
       on root do
-        res.write partial("new")
+        render :new
       end
+    end
+
+    on "test" do
+      res.write "Ajax done"
     end
   end
 
   on post do
     on root do
       on param(:quiz) do |quiz|
-        quiz["option_correct"] = "+|*" if quiz["option_correct"].length == 0
+        quiz["theme"] = "result_#{Time.now.strftime("%Y_%m_%d-%H_%M_%S")}" if quiz["theme"] == ""
+        quiz["option_correct"] = "+|*" if quiz["option_correct"] == ""
         quiz["option_correct"] = to_suffix quiz["option_correct"]
 
         questions = convert(quiz["body"], question_prefix: quiz["question_prefix"],
                                           option_prefix: quiz["option_prefix"],
                                           option_correct: quiz["option_correct"])
-        path = "/upload/result_#{Time.now.strftime("%Y_%m_%d-%H_%M_%S")}.txt"
+        path = "/upload/#{quiz["theme"]}.txt"
         result = File.open("public#{path}", "w")
         questions.each do |question|
           question.write result
         end
         result.close
 
-        res.write partial("quiz_preview", questions: questions, filename: path)
+        render :quiz_preview, questions: questions, filename: path
       end
     end
 
