a2238906eb647718e3b2b1d6cf456534141551a6
Earendil95 <ermak95@gmail.com>
2016-04-08 22:23:01 +0300

    More styles

--- public/javascript/buttons.js
+++ public/javascript/buttons.js
@@ -1,25 +1,26 @@
 function sendData() {
   $( document ).ready(function() {
     $( "#quiz-form" ).submit(function(event) {
+      $( "#submit-btn" ).button("loading")
+      $( "#download-link" ).addClass("disabled")
       data = $( "#quiz-form" ).serialize();
       $.ajax({
         type: "POST",
         url: "/",
         data: data,
         dataType: "html"
-      }).done( function (response) {
+      }).done( function(response) {
         $( "#results" ).html(response);
+        $( "#submit-btn" ).button("reset")
+        $( "#download-link" ).button("reset")
       })
       event.preventDefault();
     });
   });
 }
 
-function dropdownSelection(input, button, regexp, text) {
-  $( document ).ready(function() {
-    $( button ).click(function(event) {
-      $( input ).val(regexp)
-      $( button ).text(text)
-    });
+function tooltips() {
+  $(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
   });
 }
